7cfd5ab55d0d4d0a3017e040bf0fda63cb471a9b
WaterDr0ps <3165189679@qq.com>
2021-10-14 23:27:14 +0800

    10.14test
    bubbling

--- TestProject/src/Method.java
+++ TestProject/src/Method.java
@@ -2,9 +2,25 @@ import java.util.Scanner;
 
 public class Method {
     public static void main(String[] args) {
-        StudentObject stu=new StudentObject();
-        stu.name="王虎";
-        stu.study();
+//        StudentObject stu=new StudentObject();
+//        stu.name="王虎";
+//        stu.study();
+        Scanner sc=new Scanner(System.in);
+        int arr[]=new int[5];
+        System.out.println("请给数组遍历5个元素");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length/2;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        printArray(arr);
     }
 
     //强制类型转换
