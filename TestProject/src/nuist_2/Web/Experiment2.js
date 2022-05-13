aaa93a9eb62632d9dded0e24006506987c324ae4
WaterDr0ps <3165189679@qq.com>
2022-05-13 23:52:13 +0800

    5.13

--- TestProject/src/nuist_2/Web/Experiment2.js
+++ TestProject/src/nuist_2/Web/Experiment2.js
@@ -1,8 +1,35 @@
-<script language="javascript">
-    var x=0;
-    for(var i=0;i<=100;i++){
-        x+=i;
+function Sum(x) {
+    let sum = 0;
+    for(let i=0;i<=x;i++){
+        sum+=i;
+    }
+    document.log(sum);
 }
-    document.write(x);
 
-</script>
+function change(x) {
+    switch (x) {
+        case 0:
+            document.write("星期日");
+            break;
+        case 1:
+            document.write("星期一");
+            break;
+        case 2:
+            document.write("星期二");
+            break;
+        case 3:
+            document.write("星期三");
+            break;
+        case 4:
+            document.write("星期四");
+            break;
+        case 5:
+            document.write("星期五");
+            break;
+        case 6:
+            document.write("星期六");
+            break;
+        default:
+            document.write("输入有误");
+    }
+}
