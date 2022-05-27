e1b5c0d3102c3562661b356a2122892e54b610c2
WaterDr0ps <3165189679@qq.com>
2022-05-27 23:54:15 +0800

    5.27

---
+++ TestProject/src/nuist_2/Practice/May/May_27/Test.java
@@ -0,0 +1,39 @@
+package nuist_2.Practice.May.May_27;
+import java.lang.reflect.Field;
+import sun.misc.Unsafe;
+/**
+ * @author Eliot
+ */
+public class Test {
+
+    public static void main(String[] args) {
+//        Unsafe unsafe=Unsafe.getUnsafe();
+//        final int size=Integer.MAX_VALUE/2;
+//        final long addr=unsafe.allocateMemory(size);
+//        try {
+//            System.out.println("Unsafe address="+addr);
+//            for (int i = 0; i < size; i++) {
+//                unsafe.putByte(addr+i,(byte)123);
+//                if(unsafe.getByte(addr+i)!=123){
+//                    System.out.println("Failed at offset="+i);
+//                }
+//            }
+//        }finally {
+//            unsafe.freeMemory(addr);
+//        }
+
+//        for (int i=0;i<100000;i++){
+//            Object o=new Object();
+//        }
+//        Runtime run=Runtime.getRuntime();
+//        long max=run.maxMemory();
+//        long total=run.totalMemory();
+//        long free=run.freeMemory();
+//        long usable=max-total+free;
+//        long using=total-free;
+//        System.out.println("总内存"+total/1024/1024+"M");
+//        System.out.println("已用内存"+using/1024/1024+"M");
+//        System.out.println("可用内存"+usable/1024/1024+"M");
+//        System.out.println("空闲内存"+free/1024/1024+"M");
+    }
+}
