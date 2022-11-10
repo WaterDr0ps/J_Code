f5f7b49783ca187592addf4f186a454376286848
xwc <xwenchao7@163.com>
2022-11-10 13:30:04 +0800

    SMS key屏蔽

--- reggie_takeout/src/main/java/com/chao/utils/SMSUtils.java
+++ reggie_takeout/src/main/java/com/chao/utils/SMSUtils.java
@@ -19,7 +19,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tKbPyN1BFTfuqCfcmDy", "Wjv1YGYxyYGg0ZcAaOnfAjsctmxVGV");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
